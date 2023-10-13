50ed5ec8e054e5a35a5fa14f4761cfc5f85978da
hashir4560 <haashirraja@gmail.com>
2023-10-13 19:09:07 +0500

    Initial

---
+++ sandbox.js
@@ -0,0 +1,37 @@
+// const character="haBhhxg"
+// console.log(character)
+// const inputs=document.querySelectorAll('input')
+// console.log(inputs);
+// inputs.forEach(input =>{
+//     console.log(input)
+// })
+var character = "hashir";
+var age = 12;
+var isBlackelt = false;
+//
+character = "raja";
+age = 40;
+isBlackelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
+//Arrays 
+var names = ['hashir', 'abdullah', 'dhamir'];
+names.push('audi');
+var numbers = [20, 45, 67, 1];
+numbers.push(89);
+console.log(numbers);
+//Mixed Arrays
+var mixed = ['ken', 4, 5, 'jennx'];
+mixed.push(3);
+mixed.push("pass");
+mixed[0] = 3;
+//Objects
+var hashir = {
+    name: "hashir",
+    color: "white",
+    age: 22
+};
+hashir.age = 90;
+console.log(hashir);
---
+++ sandbox.ts
@@ -0,0 +1,56 @@
+// const character="haBhhxg"
+// console.log(character)
+
+// const inputs=document.querySelectorAll('input')
+// console.log(inputs);
+// inputs.forEach(input =>{
+//     console.log(input)
+// })
+let character="hashir"
+let age =12
+let isBlackelt=false
+//
+ character="raja"
+
+ age=40;
+
+ isBlackelt=true;
+
+ const circ= (diameter:number)=>{
+    return diameter*Math.PI
+
+ }
+console.log(circ(7.5))
+
+
+
+//Arrays 
+
+let names=['hashir','abdullah','dhamir']
+names.push('audi');
+
+
+let numbers=[20,45,67,1]
+numbers.push(89);
+console.log(numbers);
+
+
+//Mixed Arrays
+let mixed=['ken',4,5,'jennx']
+mixed.push(3);
+mixed.push("pass")
+mixed[0]=3;
+
+//Objects
+let hashir={
+    name:"hashir",
+    color:"white",
+    age:22
+
+
+}
+
+hashir.age=90;
+
+console.log(hashir)
+
